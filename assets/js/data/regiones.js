b8528ee84ab6ba1b78a4e015e9d8001ea3e409ee
Sebastian-Montupil <se.montupil@duocuc.cl>
2026-09-07 21:34:14 -0300

    feat: se agregan regiones y comunas para formularios

---
+++ assets/js/data/regiones.js
@@ -0,0 +1,179 @@
+const regiones = [
+    {
+        nombre: "Región de Arica y Parinacota",
+        comunas: [
+            "Arica",
+            "Camarones",
+            "Putre",
+            "General Lagos"
+        ]
+    },
+    {
+        nombre: "Región de Tarapacá",
+        comunas: [
+            "Iquique",
+            "Alto Hospicio",
+            "Pozo Almonte",
+            "Pica",
+            "Huara"
+        ]
+    },
+    {
+        nombre: "Región de Antofagasta",
+        comunas: [
+            "Antofagasta",
+            "Calama",
+            "Tocopilla",
+            "Mejillones",
+            "San Pedro de Atacama",
+            "Taltal"
+        ]
+    },
+    {
+        nombre: "Región de Atacama",
+        comunas: [
+            "Copiapó",
+            "Caldera",
+            "Tierra Amarilla",
+            "Vallenar",
+            "Chañaral",
+            "Huasco"
+        ]
+    },
+    {
+        nombre: "Región de Coquimbo",
+        comunas: [
+            "La Serena",
+            "Coquimbo",
+            "Ovalle",
+            "Illapel",
+            "Vicuña",
+            "Los Vilos"
+        ]
+    },
+    {
+        nombre: "Región de Valparaíso",
+        comunas: [
+            "Valparaíso",
+            "Viña del Mar",
+            "Quilpué",
+            "Villa Alemana",
+            "Concón",
+            "San Antonio",
+            "Quillota",
+            "Los Andes"
+        ]
+    },
+    {
+        nombre: "Región Metropolitana de Santiago",
+        comunas: [
+            "Santiago",
+            "Maipú",
+            "Providencia",
+            "Las Condes",
+            "Puente Alto",
+            "La Florida",
+            "Ñuñoa",
+            "San Bernardo"
+        ]
+    },
+    {
+        nombre: "Región del Libertador General Bernardo O'Higgins",
+        comunas: [
+            "Rancagua",
+            "Machalí",
+            "San Fernando",
+            "Santa Cruz",
+            "Rengo",
+            "Pichilemu"
+        ]
+    },
+    {
+        nombre: "Región del Maule",
+        comunas: [
+            "Talca",
+            "Curicó",
+            "Linares",
+            "Cauquenes",
+            "Constitución",
+            "Molina"
+        ]
+    },
+    {
+        nombre: "Región de Ñuble",
+        comunas: [
+            "Chillán",
+            "Chillán Viejo",
+            "San Carlos",
+            "Bulnes",
+            "Quirihue",
+            "Coihueco"
+        ]
+    },
+    {
+        nombre: "Región del Biobío",
+        comunas: [
+            "Concepción",
+            "Talcahuano",
+            "Los Ángeles",
+            "Chiguayante",
+            "San Pedro de la Paz",
+            "Coronel",
+            "Lota",
+            "Arauco"
+        ]
+    },
+    {
+        nombre: "Región de La Araucanía",
+        comunas: [
+            "Temuco",
+            "Padre Las Casas",
+            "Angol",
+            "Villarrica",
+            "Pucón",
+            "Victoria"
+        ]
+    },
+    {
+        nombre: "Región de Los Ríos",
+        comunas: [
+            "Valdivia",
+            "La Unión",
+            "Río Bueno",
+            "Panguipulli",
+            "Paillaco",
+            "Los Lagos"
+        ]
+    },
+    {
+        nombre: "Región de Los Lagos",
+        comunas: [
+            "Puerto Montt",
+            "Osorno",
+            "Castro",
+            "Ancud",
+            "Puerto Varas",
+            "Quellón"
+        ]
+    },
+    {
+        nombre: "Región de Aysén del General Carlos Ibáñez del Campo",
+        comunas: [
+            "Coyhaique",
+            "Aysén",
+            "Chile Chico",
+            "Cochrane",
+            "Cisnes"
+        ]
+    },
+    {
+        nombre: "Región de Magallanes y de la Antártica Chilena",
+        comunas: [
+            "Punta Arenas",
+            "Natales",
+            "Porvenir",
+            "Cabo de Hornos",
+            "Torres del Paine"
+        ]
+    }
+];
